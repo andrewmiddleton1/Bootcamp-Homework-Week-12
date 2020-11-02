6fd307fa49eb9085d89cb89f9d32b229cdd79edf
andrewmiddleton1 <66552488+andrewmiddleton1@users.noreply.github.com>
2020-11-02 20:44:02 +1000

    final files with delete function working

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,26 +1,42 @@
 const path = require("path");
 const router = require("express").Router();
-const notesDB = require("./db/db.json");
+const notesDB = require("../db/db.json");
 var fs = require("fs");
 
 
+// the get function is quite straightforward as the db.json file has already been required, 
+// so we just return the notesDB in JSON format as the response.
 router.get("/api/notes", (req, res) => {
 
-	fs.readFileSync("../db/db.json", "utf-8", (err, data) => {
-		if (err) { console.log(err) }
-		console.log(data);
-	});
-	res.json(data);
+	// const data = fs.readFileSync("./db/db.json", { encoding: "utf8" });
+	console.log(notesDB);
+	res.json(notesDB);
 });
 
+// post function which moves the request body into the notes variable
+// then makes the notes ID a unique number (in this case, using the current date/time)
+// the function pushes the new note into the notesDB array and then writes the array to the db.jsonfile
 router.post("/api/notes", (req, res) => {
-	let note = req.body
+	let note = req.body;
+	note.id = Date.now();
 	console.log(note);
-	fs.writeFileSync("../db/db.json", JSON.stringify(note), "UTF8");
+	notesDB.push(note);
+	fs.writeFileSync("./db/db.json", JSON.stringify(notesDB), "UTF8");
+	res.json(notesDB);
 });
 
-router.delete("api/notes/", (req, res) => {
-
+// the delete function checks the IDs of each of the notes against the current note chosen for delete
+// if the IDs match, we use the 'Splice' command to delete that particular element of the array.
+router.delete("/api/notes/:id", (req, res) => {
+	console.log("trying to delete:" + req.params.id);
+	const indexToDelete = notesDB.findIndex(function (currentNote) {
+		if (currentNote.id == req.params.id)
+			return true
+		else
+			return false
+	});
+	notesDB.splice(indexToDelete, 1);
+	return res.json(notesDB);
 });
 
 module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -12,62 +12,13 @@ const htmlRoutes = require("./routes/htmlRoutes");
 const app = express();
 const PORT = 3000;
 
-// Sets up the Express app to handle data parsing
+// Sets up the Express app to handle data parsing and routes
+//Note: Routes will established in their own js files
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 app.use(apiRoutes);
 app.use(htmlRoutes);
-
-//Data
-
-// let notes = [
-//     {
-//         noteID: 1234,
-//         notecontent: "First Note's Content"
-
-//     },
-//     {
-//         noteID: 5678,
-//         notecontent: "Second Note's Content"
-
-//     }
-// ];
-
-
-
-// Routes
-// =============================================================
-
-// Route setup for HTML
-// app.get("/", function (req, res) {
-//     res.sendFile(path.join(__dirname, "index.html"));
-// });
-// app.get("/notes", function (req, res) {
-//     res.sendFile(path.join(__dirname, "notes.html"));
-// });
-
-
-// // Route Setup for the API
-// app.get("/api/notes", function (req, res) {
-//     fs.readFileSync(__dirname + "/db/db.json");
-//     res.json();
-// });
-
-// app.post("/api/notes", function (req, res) {
-//     // 
-//     //notes.push(newNote);
-//     // console.log(newNote);
-//     fs.writeFileSync(__dirname + "/db/db.json", JSON.stringify(notes - array), "UTF8");
-//     // res.json(newNote);
-
-// });
-
-// We then add the json the user sent to the character array + save the array to db.json
-
-
-// app.post('/api/clear', function (req, res) {
-//     notes = [];
 
 
 
